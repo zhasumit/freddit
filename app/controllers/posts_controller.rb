7f9939e263bb684f67319008b67e7d679f8e4286
Sumit Jha <sumitzha2057@gmail.com>
2025-04-02 00:14:26 +0530

    deleting own posts, login forms improvement

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :authenticate_account!, except: [ :index, :show ]
-  before_action :set_post, only: [ :show ]
+  before_action :set_post, only: [ :show, :destroy ]  # Added :destroy here
   before_action :auth_subscriber, only: [ :new ]
+  before_action :correct_account, only: [ :destroy ]  # New authorization for delete
 
   def index
     @posts = Post.all
@@ -17,19 +18,26 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new post_values
+    @post = Post.new(post_values)
     @post.account_id = current_account.id
     @post.community_id = params[:community_id]
 
     if @post.save
-      redirect_to community_path(@post.community_id)
+      redirect_to community_path(@post.community_id), notice: "Post created successfully"
     else
       @community = Community.find(params[:community_id])
       render :new
     end
   end
 
+  def destroy
+    community_id = @post.community_id
+    @post.destroy
+    redirect_to community_path(community_id), notice: "Post deleted successfully"
+  end
+
   private
+
   def set_post
     @post = Post.includes(:comments).find(params[:id])
   end
@@ -39,6 +47,13 @@ class PostsController < ApplicationController
       redirect_to root_path, flash: { error: "You must be a member to create a post" }
     end
   end
+
+  def correct_account
+    unless current_account == @post.account
+      redirect_to root_path, alert: "You are not authorized to perform this action"
+    end
+  end
+
   def post_values
     params.require(:post).permit(:title, :body)
   end
